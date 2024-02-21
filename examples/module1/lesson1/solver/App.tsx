6c04c273025b45fe8f2a9f3ba94bbd00a33230b1
Gaba Bartnicka <bartnicka.g@gmail.com>
2024-02-21 13:14:12 +0100

    ♻️ Refactor App.tsx into smaller components
    Signed-off-by: Gaba Bartnicka <bartnicka.g@gmail.com>

--- examples/module1/lesson1/solver/App.tsx
+++ examples/module1/lesson1/solver/App.tsx
@@ -1,28 +1,7 @@
-import React, { useState } from 'react';
-import { add, substract, multiply, divide } from './functions';
-
-interface OperationButtonProps {
-  operation: (a: number, b: number) => number;
-  label: string;
-  firstNumber: number;
-  secondNumber: number;
-  result: React.Dispatch<React.SetStateAction<number | string>>;
-}
-
-const OperationButton: React.FC<OperationButtonProps> = ({ operation, label, firstNumber, secondNumber, result }) => {
-  const doWork = () => {
-    result(operation(firstNumber, secondNumber));
-  };
-
-  return (
-    <button
-      className="bg-blue-200 px-2 py-4 text-lg hover:bg-blue-500 hover:text-white rounded-md"
-      onClick={doWork}
-    >
-      {label}
-    </button>
-  );
-};
+import { useState } from 'react';
+import Result from './Result';
+import InputGrid from './InputGrid';
+import OperationButtonsGrid from './OperationButtonsGrid';
 
 const App = () => {
   const [firstNumber, setFirstNumber] = useState<number>(0);
@@ -31,27 +10,10 @@ const App = () => {
 
   return (
     <div>
-      <div className="grid grid-cols-2 gap-x-4">
-        <input
-          type="number"
-          className="rounded-md shadow-md p-4"
-          value={firstNumber}
-          onChange={(e) => setFirstNumber(parseFloat(e.target.value))}
-        />
-        <input
-          type="number"
-          className="rounded-md shadow-md p-4"
-          value={secondNumber}
-          onChange={(e) => setSecondNumber(parseFloat(e.target.value))}
-        />
-      </div>
-      <div className="grid grid-cols-4 gap-x-4 my-4">
-        <OperationButton operation={add} label="+" firstNumber={firstNumber} secondNumber={secondNumber} result={setResult} />
-        <OperationButton operation={substract} label="-" firstNumber={firstNumber} secondNumber={secondNumber} result={setResult} />
-        <OperationButton operation={multiply} label="*" firstNumber={firstNumber} secondNumber={secondNumber} result={setResult} />
-        <OperationButton operation={divide} label="/" firstNumber={firstNumber} secondNumber={secondNumber} result={setResult} />
-      </div>
-      <div>Result: {result}</div>
+      <InputGrid firstNumber={firstNumber} setFirstNumber={setFirstNumber} secondNumber={secondNumber}
+                 setSecondNumber={setSecondNumber} />
+      <OperationButtonsGrid firstNumber={firstNumber} secondNumber={secondNumber} setResult={setResult} />
+      <Result result={result} />
     </div>
   );
 };
